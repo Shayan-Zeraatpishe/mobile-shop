1bcf8925c8b9df741ecc2e30865306ccb851d3dc
Shayan-Zeraatpishe <zeraatpishe.shayan.007@gmail.com>
2025-11-11 22:27:55 +0330

    add  specila page

---
+++ js/homepage.js
@@ -0,0 +1,55 @@
+//add to cart
+const toggleButton = document.getElementById('container-card');
+const toggleButtonCart = document.getElementById('addcart');
+
+toggleButtonCart.addEventListener('click', function () {
+    toggleButton.classList.toggle('active');
+});
+
+
+
+/* Open-close navbar */
+const containerdark = document.getElementById('container-dark');
+const btnmenu = document.getElementById('btnmenu');
+const navbar = document.getElementById('navbar');
+
+btnmenu.addEventListener('click', function () {
+    containerdark.classList.toggle('active-dark');
+    navbar.classList.toggle('active-navbar');
+});
+
+containerdark.addEventListener('click', function () {
+    navbar.classList.toggle('active-navbar');
+    containerdark.classList.remove('active-dark');
+});
+
+
+
+/* Submenu */
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".btnsubment");
+
+    buttons.forEach(btn => {
+        btn.addEventListener("click", function () {
+            const li = btn.closest("li");
+            const submenu = li.querySelector(".submenu");
+            submenu.classList.toggle("active-submenu");
+        });
+    });
+});
+
+
+const btnsearch = document.getElementById('btnsearch');
+const containersearch = document.querySelector('.container-serach');
+const closecontainersearch = document.getElementById('closecontainersearch');
+btnsearch.addEventListener('click', function () {
+    containersearch.classList.add("container-serach-show");
+});
+
+closecontainersearch.addEventListener('click' , function(){
+    containersearch.classList.remove("container-serach-show");
+});
+
+
+
+
